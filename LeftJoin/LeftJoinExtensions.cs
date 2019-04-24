0d602d2ae8bcb4a5041cebdb4e67545e7da22f84
Oguzhan Kahyaoglu <oguzhan.kahyaoglu@neyasis.com>
2019-04-24 14:59:48 +0300

    v1 commit

---
+++ LeftJoin/LeftJoinExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LeftJoin
+{
+    public static class LeftJoinExtensions
+    {
+        public static IQueryable<TResult> LeftJoin<TLeft, TRight, TKey, TResult>(
+            this IQueryable<TLeft> left, 
+            IQueryable<TRight> right,
+            Expression<Func<TLeft, TKey>> leftKey,
+            Expression<Func<TRight, TKey>> rightKey,
+            Func<TLeft, TRight, TResult> resultFunc
+            )
+        {
+            /*
+            var query = (
+                    from l in left
+                    join r in right on leftKey(l) equals rightKey(r)
+                    into j1
+                    from r1 in j1.DefaultIfEmpty()
+                    select resultFunc(l, r1)
+                    );
+             */
+            var query = left
+                .GroupJoin(right, leftKey, rightKey, (l, j1) => new {l, j1})
+                .SelectMany(t => t.j1.DefaultIfEmpty(), (t, r1) => resultFunc(t.l, r1));
+            return query;
+        }
+    }
+}
