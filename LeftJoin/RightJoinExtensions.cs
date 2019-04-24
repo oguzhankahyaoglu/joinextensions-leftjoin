593b1ac9425b0bb66de67771c81167bf99770e6a
Oguzhan Kahyaoglu <oguzhan.kahyaoglu@neyasis.com>
2019-04-24 15:25:27 +0300

    rightjoin added

---
+++ LeftJoin/RightJoinExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace LeftJoin
+{
+    public static class RightJoinExtensions
+    {
+        public static IQueryable<TResult> RightJoin<TLeft, TRight, TKey, TResult>(
+            this IQueryable<TLeft> left, 
+            IQueryable<TRight> right,
+            Expression<Func<TLeft, TKey>> leftKey,
+            Expression<Func<TRight, TKey>> rightKey,
+            Func<TLeft, TRight, TResult> resultFunc
+        )
+        {
+            var query = right.LeftJoin(left, rightKey, leftKey, (i, o) => resultFunc(o, i));
+            return query;
+        }
+
+    }
+}
